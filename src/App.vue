e14c8bd2f4265eafcf894927ace68af94bf3022f
Mateusz Różalski <olivetum@gmail.com>
2017-11-07 11:37:06 +0100

    Set basic porfolio layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
     <Header/>
+
+    <Portfolio/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Portfolio from './components/Portfolio.vue'
+
 export default {
   name: 'app',
-  components: { Header },
+  components: { Header, Portfolio },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="portfolio">
+   <h2>Portfolio <span class="txt4">/</span> Showcase</h2>
+   <div class="works">
+     <div class="work-box"></div>
+     <div class="work-box"></div>
+   </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Hello'
+      }
+    }
+  }
+</script>
